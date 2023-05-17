49d16ee8c25f98fb28b04288fb754fd847147914
udaysai1311 <udaysai1311@gmail.com>
2023-05-17 10:16:00 +0530

    Route matcher example

--- src/params/integer.js
+++ src/params/integer.js
@@ -2,5 +2,5 @@ export function match(param){
     return /^\d+$/.test(param);
 }
 
-// catch all routes
-// routes folder => create a folder or edit the existing [catch all route folder] as example-'[productId=integer]'
+// Route Matchers example
+// routes folder => create a folder or edit the existing [dynamic route folder] as example-'[productId=integer]'
