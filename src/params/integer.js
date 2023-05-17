82c79057294f106f630546bf32a0185999659267
udaysai1311 <udaysai1311@gmail.com>
2023-05-17 10:13:22 +0530

    match param int example

--- src/params/integer.js
+++ src/params/integer.js
@@ -1,3 +1,6 @@
 export function match(param){
     return /^\d+$/.test(param);
 }
+
+// catch all routes
+// routes folder => create a folder or edit the existing [catch all route folder] as example-'[productId=integer]'
