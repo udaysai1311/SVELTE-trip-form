3899574bece5a16b41529bdfb0b8dd02a30000b0
udaysai1311 <udaysai1311@gmail.com>
2023-05-17 10:44:52 +0530

    Catch all routes example

---
+++ src/routes/docs/[...slug]/+page.svelte
@@ -0,0 +1,21 @@
+<!--CATCH ALL ROUTES EXAMPLE-->
+
+
+<!--folder [...(slug)=> this can be any name ]-->
+
+<!--
+    to catch the parameters from the url 
+-->
+
+<script>
+    import {page} from '$app/stores'
+	import { merge_ssr_styles } from 'svelte/internal';
+    console.log(page.params.slug) // it will log the url parameters in the console so to catch these params we need to do this .....
+    const slugArr =  $page.params.slug.split("/")
+</script>
+
+{#if slugArr.length === 1}
+    <h1>{slugArr[0]}</h1>
+{:else}
+    <h1>{slugArr[0] slugArr[1]}</h1>
+{/if} 
